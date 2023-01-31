ac8d55e721ed93483c1ac80c3226a6471cfd7ed8
Uliana Koneva <loka.koneva@gmail.com>
2023-01-31 12:20:22 +0500

    Проведен рефакторинг бизнес-логики.

--- get_news_information_project/services/news.py
+++ get_news_information_project/services/news.py
@@ -5,7 +5,7 @@ from typing import List, Union
 from fastapi import status, HTTPException
 
 from ..models.input_data import InputNewsData, InputCommentsData
-from ..models.news import NewsOutput, OutputNewsData, NewsOutputById
+from ..models.news import NewsOutput, NewsInput, OutputNewsData, NewsOutputById
 from ..models.comments import CommentOutput
 from ..settings import settings
 
@@ -24,7 +24,7 @@ async def get_comments_from_files() -> InputCommentsData:
     return InputCommentsData.parse_raw(content_comments)
 
 
-def get_comments_by_news_id(
+def _get_comments_by_news_id(
         news_id: int,
         comments: InputCommentsData
 ) -> Union[List[CommentOutput], list]:
@@ -34,31 +34,49 @@ def get_comments_by_news_id(
     return list(comments)
 
 
-async def get_output_news():
-    input_news = await get_news_from_files()
-    input_comments = await get_comments_from_files()
+def _get_output_news_by_id(
+        news_id: int,
+        input_news: InputNewsData
+) -> Union[List[NewsInput], list]:
+    output_news = filter(lambda x: x.id == news_id, input_news.news)
+    return list(output_news)
+
+
+def _get_output_news(
+        input_news: InputNewsData,
+        input_comments: InputCommentsData
+) -> Union[List[NewsOutput], list]:
     output_news = []
     for news in input_news.news:
         if not news.deleted and datetime.now() > news.date:
-            comments = get_comments_by_news_id(news.id,  input_comments)
-            result = NewsOutput(**news.dict(), comments_count=len(list(comments)))
+            comments = _get_comments_by_news_id(news.id, input_comments)
+            result = NewsOutput(**news.dict(), comments_count=len(comments))
             output_news.append(result)
-    output_news.sort(key=lambda x: x.date)
-
-    return OutputNewsData(news=output_news, news_count=len(output_news))
+    return output_news
 
 
-async def get_output_news_by_id(news_id):
-    input_news = await get_news_from_files()
-    input_comments = await get_comments_from_files()
-    output_news_list = list(filter(lambda x: x.id == news_id, input_news.news))
-
+def check_exception(output_news_list: Union[List[NewsInput], list]):
     if len(output_news_list) == 0 \
             or output_news_list[0].deleted \
             or output_news_list[0].date > datetime.now():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-    output_comments = get_comments_by_news_id(news_id,  input_comments)
+
+async def get_output_news() -> OutputNewsData:
+    input_news = await get_news_from_files()
+    input_comments = await get_comments_from_files()
+    output_news = _get_output_news(input_news, input_comments)
+    output_news.sort(key=lambda x: x.date)
+
+    return OutputNewsData(news=output_news, news_count=len(output_news))
+
+
+async def get_output_news_by_id(news_id) -> NewsOutputById:
+    input_news = await get_news_from_files()
+    input_comments = await get_comments_from_files()
+    output_news_list = _get_output_news_by_id(news_id, input_news)
+    check_exception(output_news_list)
+    output_comments = _get_comments_by_news_id(news_id,  input_comments)
     output_comments.sort(key=lambda x: x.date)
 
     return NewsOutputById(**output_news_list[0].dict(),
